58b53bab5a12509acd80fbfc41312095f08adf23
“candielazhu” <candie_lazhu@163.com>
2025-12-30 21:44:23 +0800

    feat(用户管理): 添加用户电话和简介字段及管理员搜索功能
    - 在用户表中新增phone和bio字段
    - 创建hard-pig-16搜索组件
    - 在管理员页面集成搜索功能

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,5 +1,21 @@
 <template>
     <div>
-        <h1>管理员中心</h1>
+        <div>
+            <h1>管理员中心</h1>
+        </div>
+        <div>
+            <div>
+                用户名搜索
+                <hardPig16 />
+            </div>
+            <div>
+                
+                <hardPig16 />
+            </div>
+        </div>
     </div>
 </template>
+
+<script setup>
+import hardPig16 from './hard-pig-16.vue'
+</script>
